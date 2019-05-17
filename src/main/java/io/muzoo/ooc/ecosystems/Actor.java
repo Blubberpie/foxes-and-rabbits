051d0d4f6981fadcbf8e575240374d3b2876675c
blubberpie <lozberipie@gmail.com>
2019-05-17 12:30:41 +0700

    Wrote action template for Hunter class. Currently does nothing.

--- src/main/java/io/muzoo/ooc/ecosystems/Actor.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Actor.java
@@ -2,7 +2,43 @@ package io.muzoo.ooc.ecosystems;
 
 public abstract class Actor extends LifeForm {
 
-    // The actor's hunger which
+    // The actor's fullness which
     // gets
-    private int hunger;
+    private int fullness;
+
+    // -1 = Hungry
+    // 0 = Neutral
+    // 1 = Full
+    private int hungerStatus;
+
+    public void incrementFullness(int hungerBoost, int HUNGER_THRESHOLD){
+        fullness += hungerBoost;
+        setHungerStatus(HUNGER_THRESHOLD);
+    }
+
+    public void decrementFullness(int MAX_DEPLETION_RATE, int HUNGER_THRESHOLD){
+        fullness -= (MAX_DEPLETION_RATE * (1 - (getFullness() / HUNGER_THRESHOLD)));
+        setHungerStatus(HUNGER_THRESHOLD);
+    }
+
+    public void setHungerStatus(int HUNGER_THRESHOLD){
+        if (fullness > (HUNGER_THRESHOLD * 0.8)) {
+            this.hungerStatus = 1;
+//            this.fullness = (int) Math.round(HUNGER_THRESHOLD * 0.8);
+            this.fullness = HUNGER_THRESHOLD;
+        }
+        else if (fullness < (HUNGER_THRESHOLD * 0.2)) {
+            this.hungerStatus = -1;
+//            this.fullness = (int) Math.round(HUNGER_THRESHOLD * 0.2);
+            this.fullness = 0;
+        }
+        else this.hungerStatus = 0;
+    }
+
+
+    public int getFullness() { return fullness; }
+
+    public int getHungerStatus() { return hungerStatus; }
+
+    public void setFullness(int fullness) { this.fullness = fullness; }
 }
--- src/main/java/io/muzoo/ooc/ecosystems/Hunter.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Hunter.java
@@ -1,10 +1,36 @@
 package io.muzoo.ooc.ecosystems;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-public abstract class Hunter extends Actor{
+public class Hunter extends Actor{
+
+    private static final int MAX_DEPLETION_RATE = 5;
+    private static final int HUNGER_THRESHOLD = 50;
 
     // Mapping of a hunter's possible targets
     // and their hunger-filling value.
     private static Map<Class, Integer> VICTIMS;
+
+    public Hunter() {
+        setFullness(0);
+        setHungerStatus(HUNGER_THRESHOLD);
+        VICTIMS = new LinkedHashMap<Class, Integer>(){{
+            put(Tiger.class, 15);
+            put(Fox.class, 8);
+            put(Rabbit.class, 4);
+        }};
+    }
+
+    public void act(Field currentField, Field updatedField){
+        decrementFullness(MAX_DEPLETION_RATE, HUNGER_THRESHOLD);
+        if(getHungerStatus() == -1){
+
+        }else if(getHungerStatus() == 0){
+
+        }else if(getHungerStatus() == 1){
+
+        }
+    }
 }
